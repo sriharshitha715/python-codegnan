a4f0882868825be6212654aa7625c7ba60836656
sriharshitha715 <sriharshitha715@gmail.com>
2026-04-02 22:33:15 +0530

    Add files via upload

---
+++ conditional statments.py
@@ -0,0 +1,67 @@
+#zero or non-zero
+num = int(input("Enter a number: "))
+if num == 0:
+    print("The number is zero")
+else:
+    print("The number is non-zero")
+#postive or negative
+num=int(input("enter a number:"))
+if num<2:
+    print("the number is positive")
+else:
+    print("the number is negative")
+#even or odd
+num=int(input("enter a number:"))
+if num%2==0:
+   print("the number is even")
+else:
+    print("the number is odd")
+#check whether the person is eligible to vote or not
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    print("You are eligible to vote")
+else:
+    print("You are not eligible to vote")
+#check whether the number is divisible by 3
+num = int(input("Enter a number: "))
+
+if num % 3 == 0:
+    print("The number is divisible by 3")
+else:
+    print("The number is not divisible by 3")
+#number is divisible with 3 and 5 or not
+num=int(input("enter the number:"))
+if num%3==0:
+    if num%5==0:
+        print("the number is divisible by 3")
+    else:
+        print("the number is divisible by 5")
+#print the smaller or bigger number
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+if a > b:
+    print("First number is bigger")
+else:
+    print("Second number is bigger or equal")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
