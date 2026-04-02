c7032fbc7dfc3588f3af2a502dd59ac0532a0561
sriharshitha715 <sriharshitha715@gmail.com>
2026-04-02 14:07:31 +0530

    Add files via upload

---
+++ day3.py
@@ -0,0 +1,31 @@
+#And operator
+a = 2
+b = 1
+print(a &b)
+
+# OR operator
+c = 3
+d = 5
+print(c or d)
+
+# Left shift
+print(10<<2)
+
+#Right shift
+print(21>>2)
+
+# Member ship operators
+print(2 in [1,3,4])
+print(2 not in [1,3,4])
+
+# Identity operators
+
+print(2 is [1,3,4])
+print(2 is not [1,3,4])
+
+# using delete key word
+
+e = 21
+f = 1
+del e
+print(e+f)
